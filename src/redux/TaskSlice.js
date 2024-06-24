3bd4214fdf5be1d20ea05b97dc515b771043544f
Ashish <rbzashu1@gmail.com>
2024-06-24 09:36:04 +0530

    added slice for tasks and combine the reducer

---
+++ src/redux/TaskSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+    editTask: (state, action) => {
+      return state.map((task) =>
+        task.id === action.payload.id ? { ...task, ...action.payload } : task
+      );
+    },
+    moveTask: (state, action) => {
+      const { taskId, direction } = action.payload;
+      return state.map((task) => {
+        if (task.id === taskId) {
+          let newStage = task.stage;
+          if (direction === "forward" && newStage < 3) newStage++;
+          if (direction === "backward" && newStage > 0) newStage--;
+          return { ...task, stage: newStage };
+        }
+        return task;
+      });
+    },
+
+    deleteTask: (state, action) => {
+      return state.filter((task) => task.id !== action.payload);
+    },
+  },
+});
+
+export const { addTask, editTask, moveTask, deleteTask } = taskSlice.actions;
+export default taskSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import tasksReducer from "./TaskSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    tasks: tasksReducer,
   },
 });
 
