ecc1865e47490190a38b69b4e39baae8948f39c5
Ashish <rbzashu1@gmail.com>
2024-06-26 10:09:36 +0530

    implmented delete by drag n drop, left with trash

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -1,12 +1,20 @@
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Droppable } from "react-beautiful-dnd";
 
 const Trash = () => {
   return (
-    <Button variant="danger" className="d-flex align-items-center">
-      <i className="fas fa-trash-alt fa-3x"></i>
-    </Button>
+    <Droppable droppableId="trash">
+      {(provided) => (
+        <div ref={provided.innerRef} {...provided.droppableProps}>
+          <Button variant="danger" className="d-flex align-items-center">
+            <i className="fas fa-trash-alt fa-3x"></i>
+          </Button>
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
 
--- src/pages/TaskManagement.jsx
+++ src/pages/TaskManagement.jsx
@@ -4,14 +4,27 @@ import TaskBoard from "../components/TaskBoard";
 import TaskForm from "../components/TaskForm";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { editTask } from "../redux/TaskSlice";
+import { deleteTask, editTask } from "../redux/TaskSlice";
 import Trash from "../components/Trash";
+import { useState } from "react";
 
 const TaskManagement = () => {
+  const [isDragging, setIsDragging] = useState(false);
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
 
+  const onDragStart = () => {
+    console.log(isDragging + "in drag start before true");
+
+    setIsDragging(true);
+    console.log(isDragging + "in drag start after true");
+  };
   const onDragEnd = (result) => {
+    console.log(isDragging + "in drag start before false");
+
+    setIsDragging(false);
+    console.log(isDragging + "in drag start after false");
+
     console.log(result);
     const { source, destination } = result;
     if (!destination) {
@@ -23,9 +36,20 @@ const TaskManagement = () => {
     ) {
       return;
     }
+
     const stages = ["Backlog", "To Do", "Ongoing", "Done"];
 
     const data = tasks.filter((task) => task.name === result.draggableId);
+    if (destination.droppableId === "trash") {
+      const confirmDelete = window.confirm(
+        `Are you sure you want to delete the task "${data[0].name}"?`
+      );
+      if (confirmDelete) {
+        dispatch(deleteTask(data[0].id));
+      }
+      return;
+    }
+    console.log(data);
 
     dispatch(
       editTask({
@@ -39,7 +63,7 @@ const TaskManagement = () => {
     <div style={{ position: "relative", minHeight: "100vh" }}>
       <NavBar />
       <TaskForm />
-      <DragDropContext onDragEnd={onDragEnd}>
+      <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
         <TaskBoard />
 
         <div
